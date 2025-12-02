77d88b8d18f870533c4131d340db060257bc62cf
Khaled Gawish <khaled.gawesh@gmail.com>
2025-12-02 11:12:11 +0200

    first edit

---
+++ 0001.py
@@ -0,0 +1,23 @@
+# int 
+x = 5
+
+# float
+float = 1.5
+
+# list
+list = [4, 5, 6]
+
+# set
+set = {2, 3, 4, 5}
+
+
+# tuple
+
+tupp = (10, 20, 30)
+
+# dict
+diiii = {"name": "ahemd", "age" : 20}
+
+# bool
+x = True 
+
